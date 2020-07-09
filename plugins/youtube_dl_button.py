6493cee2c0f88e2a4b92acde0df82b15510832f1
Kakarotto22 <57814819+Kakarotto22@users.noreply.github.com>
2020-07-09 13:55:35 +0300

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -109,7 +109,7 @@ async def youtube_dl_call_back(bot, update):
     tmp_directory_for_each_user = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id)
     if not os.path.isdir(tmp_directory_for_each_user):
         os.makedirs(tmp_directory_for_each_user)
-    download_directory = tmp_directory_for_each_user + "/" + custom_file_name
+    download_directory = tmp_directory_for_each_user + "/" + "custom_file_name"
     command_to_exec = []
     if tg_send_type == "audio":
         command_to_exec = [
